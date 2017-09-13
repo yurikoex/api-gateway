9ea4542763138c634832fc9acad61905b0b1a7f1
Joshua Lunsford <josh_lunsford@homedepot.com>
2017-09-13 13:42:08 -0400

    clean up

--- src/endpoints/index.js
+++ src/endpoints/index.js
@@ -1,34 +1,40 @@
 import Wreck from 'wreck'
 
+const mapUri = ({ logger, service }) => (req, cb) => {
+	logger.next([`h2o2 headers`, req.headers])
+	cb(null, service.config.uri, req.headers)
+}
+
+const onResponse = logger => (err, res, req, reply, settings, ttl) => {
+	if (err) {
+		logger.next([`h2o2 error`, err])
+		return reply(err)
+	} else {
+		logger.next(`h2o2 successful`)
+		Wreck.read(res, { json: true }, (err, payload) => {
+			reply(payload).headers = res.headers
+		})
+	}
+}
+
+const mapService = logger => service => {
+	logger.next([`creating service`, service])
+	return {
+		method: service.config.method,
+		path: service.route,
+		handler: {
+			proxy: {
+				mapUri: mapUri({ logger, service }),
+				onResponse: onResponse(logger)
+			}
+		}
+	}
+}
+
 export const createEndpoints = ({ logger, data }) => {
 	logger.next(`creating dynamic routes`)
 	return data.getServices().then(services => {
 		logger.next([`get services`, services])
-		return services.map(service => {
-			logger.next([`creating service`, service])
-			return {
-				method: service.config.method,
-				path: service.route,
-				handler: {
-					proxy: {
-						mapUri: (req, cb) => {
-							logger.next([`h2o2 headers`, req.headers])
-							cb(null, service.config.uri, req.headers)
-						},
-						onResponse: (err, res, req, reply, settings, ttl) => {
-							if (err) {
-								logger.next([`h2o2 error`, err])
-								return reply(err)
-							} else {
-								logger.next(`h2o2 successful`)
-								Wreck.read(res, { json: true }, (err, payload) => {
-									reply(payload).headers = res.headers
-								})
-							}
-						}
-					}
-				}
-			}
-		})
+		return services.map(mapService(logger))
 	})
 }
